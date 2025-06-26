a43b193aa5d91754ff6e9612ded8e7baace31350
Gaël Louage <gaellouage@hotmail.com>
2025-06-26 08:43:17 +0200

    Abbreviate a Two Word Name

---
+++ AbbreviateATwoWordNameEX/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(AbbrevName("Sam Harris Qui"));
+        Console.WriteLine(AbbrevName("Patrick Feenan"));
+        Console.WriteLine(AbbrevName("Evan Cole"));
+    }
+    public static string AbbrevName(string name)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < name.Trim().Length; i++) 
+        {
+            if(i == 0)
+            {
+                sb.Append($"{char.ToUpper(name[i])}.");
+            }
+            else if (name[i] == ' ')
+            {
+                sb.Append($"{char.ToUpper(name[i+1])}.");
+            }
+        }
+        return sb.ToString().Trim('.');
+    }
+    public static string AbbrevNameLinq(string name) =>
+        string.Join(".",name.Split(' ').Select((x,i) => char.ToUpper(x[0])));
+    
+}
